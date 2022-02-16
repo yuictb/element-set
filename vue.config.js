c01d5cde052ab79ad2d1cf47aac89251e1ab5070
潘鹏 <209595108@qq.com>
2022-02-16 10:57:53 +0800

    配置文件

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+// element-plus 按需导入
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+module.exports = {
+  // element-plus 按需导入
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+  pluginOptions: {
+    // less自动注入
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [
+        path.join(__dirname, "./src/assets/styles/variables.less"),
+        path.join(__dirname, "./src/assets/styles/mixins.less"),
+      ],
+    },
+  }
+};
+
